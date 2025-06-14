db9e42142de7ed35ff419b120f625209cc783f89
BikashPandeya <bikashpandeya5@gmail.com>
2025-06-14 21:58:09 +0545

    setting up cloudinary

--- MEN/Google_Drive_Code/routes/index.routes.js
+++ MEN/Google_Drive_Code/routes/index.routes.js
@@ -6,4 +6,29 @@ router.get('/home' , (req , res) => {
     res.render('home')
 })
 
+
+app.post("/upload-file", upload.single("file"), async (req, res) => {
+    try {
+      const fileBuffer = req.file.buffer;
+  
+      const stream = cloudinary.uploader.upload_stream(
+        { resource_type: "auto" },
+        (error, result) => {
+          if (error) {
+            console.error("Upload error:", error);
+            return res.status(500).send("Upload Failed");
+          }
+  
+          console.log("File uploaded:", result.secure_url);
+          return res.send(`File uploaded successfully: <a href="${result.secure_url}" target="_blank">View File</a>`);
+        }
+      );
+  
+      Readable.from(fileBuffer).pipe(stream);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send("Something went wrong");
+    }
+  });
+
 module.exports = router
