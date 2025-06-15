c3377c6f8ad5aa3c637f670c71e2a76dbd78aaec
BikashPandeya <bikashpandeya5@gmail.com>
2025-06-15 14:24:14 +0545

    final

--- MEN/Google_Drive_Code/app.js
+++ MEN/Google_Drive_Code/app.js
@@ -24,7 +24,8 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public"))); // if needed
 app.use("/" , indexRouter);
 
-app.listen(3000 , () => {
-    console.log("Server is running on port 3000");
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
