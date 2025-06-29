ad3c285fa7094a633b40b719b72afc73ca893737
BikashPandeya <bikashpandeya5@gmail.com>
2025-06-30 00:08:01 +0545

    Completing the fundamentals of socket.io

--- Socket-IO/index.js
+++ Socket-IO/index.js
@@ -1,3 +1,4 @@
+//1.Imports
 import express from "express"
 import http from "http"
 import { fileURLToPath } from "url"
@@ -5,10 +6,40 @@ import { dirname , join } from "path"
 import {Server } from "socket.io"
 
 
-//Instances
+//2.Instances
 const app= express()
 const server = http.createServer(app)
 const io = new Server(server)
 
 
-//Serving HTML File
+//3.Serving HTML File
+const __dirname = dirname(fileURLToPath(import.meta.url))
+// console.log(__dirname , "index.html")
+app.get("/" , (req,res) => {
+    res.sendFile(join(__dirname , "index.html"))
+})
+
+
+// 4.Define a connection event handler
+io.on('connection' , (client) => {
+    console.log("User connected to the server ✅")
+    
+    //Emit a 'message' event to the client
+    client.emit('real-time-message' , "Server is sending this data to the client")
+
+    client.on("disconnect" , () => {
+        console.log('User Disconnected From The Server ❌ ')
+    })
+
+    client.on("new message" , (message) => {
+        console.log(message)
+    })
+})
+
+
+
+//5.Start A Server
+const PORT = 3000
+server.listen(PORT , () => {
+    console.log("App running on port" , PORT)
+})
